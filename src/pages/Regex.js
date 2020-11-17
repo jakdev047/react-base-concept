2eec91f41b16fe9e8530b24119ea11fb98025fd5
jakdev047 <jakfreelancer047@gmail.com>
2020-11-17 22:33:13 +0600

    some example

--- src/pages/Regex.js
+++ src/pages/Regex.js
@@ -47,55 +47,67 @@ const Regex = () => {
     re = /hello/i;
     str1 = "Hello World";
 
-/* 
-    / REGEX_CHECK / 
-    i = case sensetive remove =>     / REGEX_CHECK /i
-
-    Meta character
-    ==============
-    ^ = must start with REGEX_CHECK string => /^REGEX_CHECK /i
-    $ = must end with REGEX_CHECK string => /^REGEX_CHECK$/i
-    . = matches any one character => /^REGEX.CHECK$/i  example: REGEX1CHECK
-    * = more times => /^REGEX*CHECK$/i  example: REGEX123CHECK
-    ? = optional => /^REGEX?CHECK$/i  example: REGEXCHECK (X or not other wise false  REGEYCHECK => return false) 
-
-
-    Character Sets
-    ===============
-    [abc] = a or b or c
-    [^abc] = a b c without
-    [0-9] = 0 to 9
-    [a-z] = a to z
-    [^0-9] = 0 to 9 without
-    [^a-z] = a to z without
-    /^[A-Z]/ = First letter must uppercase
-    (ab|bc) = ab or bc match
-
-    Quantifier and Grouping
-    =======================
-    /^el{3}/ = elll(true) ell(false)
-    /^el{2,5}/ = 2 to 5 times elll(true) el(false)
-    /^([0-9]){3}/ = 0 to 9  3times
-    /^01[0-9]{9}$/ = bangladeshi phone number check
-
-    Shorthand Characters
-    ====================
-    /\w/ = word character
-    /\W/ = word without character
-    /\d/ = digit character
-    /\D/ = digit without character
-    /\s/ = space character
-    /\S/ = space without character
-    /\b/ = word boundary character  example: /hello\b/  hello worlde (true) hellowo (false)
-    /\B/ = word boundary without character
-    /\o/ = null
-    /\n/ = new line
-
-    Assertions
-    ===========
-    /x(?=y)/  =  xy (true) x(false)
-
-*/
+    /* 
+        / REGEX_CHECK / 
+        i = case sensetive remove =>     / REGEX_CHECK /i
+    
+        Meta character
+        ==============
+        ^ = must start with REGEX_CHECK string => /^REGEX_CHECK /i
+        $ = must end with REGEX_CHECK string => /^REGEX_CHECK$/i
+        . = matches any one character => /^REGEX.CHECK$/i  example: REGEX1CHECK
+        * = more times => /^REGEX*CHECK$/i  example: REGEX123CHECK
+        ? = optional => /^REGEX?CHECK$/i  example: REGEXCHECK (X or not other wise false  REGEYCHECK => return false) 
+    
+    
+        Character Sets
+        ===============
+        [abc] = a or b or c
+        [^abc] = a b c without
+        [0-9] = 0 to 9
+        [a-z] = a to z
+        [^0-9] = 0 to 9 without
+        [^a-z] = a to z without
+        /^[A-Z]/ = First letter must uppercase
+        (ab|bc) = ab or bc match
+    
+        Quantifier and Grouping
+        =======================
+        /^el{3}/ = elll(true) ell(false)
+        /^el{2,5}/ = 2 to 5 times elll(true) el(false)
+        /^([0-9]){3}/ = 0 to 9  3times
+        /^01[0-9]{9}$/ = bangladeshi phone number check
+    
+        Shorthand Characters
+        ====================
+        /\w/ = word character
+        /\W/ = word without character
+        /\d/ = digit character
+        /\D/ = digit without character
+        /\s/ = space character
+        /\S/ = space without character
+        /\b/ = word boundary character  example: /hello\b/  hello worlde (true) hellowo (false)
+        /\B/ = word boundary without character
+        /\o/ = null
+        /\n/ = new line
+    
+        Assertions
+        ===========
+        /x(?=y)/  =  xy (true) x(false)
+    
+        Example
+        ========
+        1. /^(\+)?(88)?01[0-9]{9}$/  // phone number check
+        2. /^[0-9]{4}$/  // postal code check
+        3. /^([a-zA-Z0-9]\.?)+[^.\]@([a-zA-Z0-9]\.?)+[^.\]$/   // email check example: abc04.learn@gmail.com
+    
+        if( !/^[A-Z0-9._%+-]+@[A-Z]+\.[A-Z]{2,4}$/i.test(values.email) ) {
+            errors.email = 'Invalid Email Valid';
+         }
+    
+    
+    
+    */
 
 
 
