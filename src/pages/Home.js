f4eb2e8020478597324998485850ec0c63b21665
jakdev047 <jakfreelancer047@gmail.com>
2020-12-21 21:58:43 +0600

    to add animate css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import TopNavigation from '../components/TopNavigation/TopNavigation';
 
 import $ from "jquery";
 
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+
 // images
 import triangleImg from "../assets/images/triangle.png";
 import mackbookImg from "../assets/images/macbook.png";
@@ -13,49 +16,49 @@ class Home extends Component {
     onScrollFunc = () => {
         // Detect request animation frame
         var scroll = window.requestAnimationFrame ||
-                    // IE Fallback
-                    function(callback){ window.setTimeout(callback, 1000/60)};
-        var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
-    
+            // IE Fallback
+            function (callback) { window.setTimeout(callback, 1000 / 60) };
+        var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
         function loop() {
-    
-          elementsToShow.forEach(function (element) {
-            if (isElementInViewport(element)) {
-              element.classList.add('is-visible');
-            } else {
-              element.classList.remove('is-visible');
-            }
-          });
-    
-          scroll(loop);
+
+            elementsToShow.forEach(function (element) {
+                if (isElementInViewport(element)) {
+                    element.classList.add('is-visible');
+                } else {
+                    element.classList.remove('is-visible');
+                }
+            });
+
+            scroll(loop);
         }
-    
+
         // Call the loop for the first time
         loop();
-    
+
         function isElementInViewport(el) {
-          // special bonus for those using jQuery
-          if (typeof $ === "function" && el instanceof $) {
-            el = el[0];
-          }
-          var rect = el.getBoundingClientRect();
-          return (
-            (rect.top <= 0
-              && rect.bottom >= 0)
-            ||
-            (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-              rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-            ||
-            (rect.top >= 0 &&
-              rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-          );
+            // special bonus for those using jQuery
+            if (typeof $ === "function" && el instanceof $) {
+                el = el[0];
+            }
+            var rect = el.getBoundingClientRect();
+            return (
+                (rect.top <= 0
+                    && rect.bottom >= 0)
+                ||
+                (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+                    rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+                ||
+                (rect.top >= 0 &&
+                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+            );
         }
-      }
-    
-      // real-time event
-      componentDidMount(){
-        window.addEventListener('scroll',this.onScrollFunc);
-      }
+    }
+
+    // real-time event
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScrollFunc);
+    }
     render() {
         return (
             <Fragment>
@@ -66,15 +69,29 @@ class Home extends Component {
                         <li><Link to="/transion-effect">Transtion Effect</Link></li>
                         <li><Link to="/registration">Registration</Link></li>
                     </ul>
-                    <div class="rotate-animation">
-                        <img src={triangleImg} alt="effects"/>
+                    <div className="rotate-animation">
+                        <img src={triangleImg} alt="effects" />
                     </div>
                 </div>
-                <div style={{background: "#ddd",height: "100vh"}}></div>
+                <div style={{ background: "#ddd", height: "100vh" }}></div>
                 <div className="mackbook-section text-center show-on-scroll">
-                    <img src={mackbookImg} alt="mackbook"/>
+                    <img src={mackbookImg} alt="mackbook" />
                 </div>
-                <div style={{background: "#ddd",height: "100vh"}}></div>
+                <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
+                    <div style={{ background: "#ddd", height: "100vh" }}></div>
+                </ScrollAnimation>
+
+                <ScrollAnimation delay={4000}
+                    animateIn='tada'
+                    initiallyVisible={true}>
+                    <div style={{ background: "#eb1", height: "100vh" }}></div>
+                </ScrollAnimation>
+
+                <ScrollAnimation animateIn='bounceInRight'
+                    animateOut='bounceOutLeft'>
+                    <div style={{ background: "#eb1b33", height: "100vh" }}></div>
+                </ScrollAnimation>
+
             </Fragment>
         )
     }
